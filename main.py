6b7b1894f3fd8f7d0be6a43099cf2c3bbc26f94e
samkellerhals <kellerhalssamuel@gmail.com>
2020-03-01 02:28:56 +0100

    feedforward network training

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import torchvision
 from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
 # Download MNIST dataset
-train = datasets.QMNIST("", train=True, download=True, transform=(transforms.Compose(
-    [transforms.CenterCrop(10),
-    transforms.ToTensor()
-    ])))
+train = datasets.QMNIST("", train=True, download=True, transform=(transforms.Compose([transforms.ToTensor()])))
 
-test = datasets.QMNIST("", train=False, download=True, transform=(transforms.Compose([
-    transforms.ToTensor()])))
+test = datasets.QMNIST("", train=False, download=True, transform=(transforms.Compose([transforms.ToTensor()])))
+
+# Load data
+train_data = torch.utils.data.DataLoader(train, batch_size=12, shuffle=True)
+
+test_data = torch.utils.data.DataLoader(test, batch_size=64, shuffle=True)
 
 # Feedforward neural network
 class FeedforwardNet(nn.Module):
@@ -21,27 +24,47 @@ class FeedforwardNet(nn.Module):
         self.fc1 = nn.Linear(28*28, 56)
         self.fc2 = nn.Linear(56, 56)
         self.fc3 = nn.Linear(56, 56)
-        self.output = nn.Linear(56, 9)
+        self.fc4 = nn.Linear(56, 10)
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = F.log_softmax(self.output(x))
-        return x
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)
 
 # Initialise model
-model = FeedforwardNet()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = FeedforwardNet().to(device)
 
-# Create test data
-test_data = torch.Tensor(28,28).view(28*28)
-test_pass = model.forward(test_data)
+# Model training
+optimiser = optim.SGD(model.parameters(), lr=0.001)
 
+for epoch in range(30):
+    for data in train_data:
+        X, y = data
+        X, y = X.to(device), y.to(device)
+        model.zero_grad()
+        output = model(X.view(-1, 28*28))
+        loss = F.nll_loss(output, y)
+        loss.backward()
+        optimiser.step()
+    print(loss)
 
+# Test accuracy
 
+correct = 0
+total = 0
 
+with torch.no_grad():
+    for data in test_data:
+        X, y = data
+        X, y = X.to(device), y.to(device)
+        output = model(X.view(-1,784))
         
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
 
-
-        
-        
+print("Accuracy: ", round(correct/total, 3))
